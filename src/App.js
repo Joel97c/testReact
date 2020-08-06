07b67e80367862874e6c93b064ec1dbed7c29f4b
Joel Rivera <wellin12.jr0497c@gmail.com>
2020-08-06 16:15:30 -0500

    add:
    testeo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Navigations from './components/Navigations'
+
+function App() {
+  return (
+    <div className="App">
+  <Navigations/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigations.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+class Navigations extends Component{
+render(){
+    return(
+<nav className="navbar navbar-dark bg-dark">
+    <ul>
+    <li className="text-white">test</li>
+    <li className="text-white">test</li>
+    <li className="text-white">test</li>
+    </ul>
+</nav>
+    )
+ }
+}
+export default Navigations;
